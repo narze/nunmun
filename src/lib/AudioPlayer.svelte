9402460a72a0e1d212c13ed6f9dcba265af0cac6
nattapong.ja <nattapong.jaisabai@outlook.com>
2022-10-26 15:13:50 +0700

    feat: use tts-api to read dialogs

---
+++ src/lib/AudioPlayer.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+
+  export let src: string
+
+  let player: HTMLAudioElement
+</script>
+
+{#if src}
+  <div class="relative">
+    <audio bind:this={player} {src} autoplay></audio>
+  </div>
+{/if}
